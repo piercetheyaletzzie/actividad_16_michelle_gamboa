2b9d9132c7567466c186a65e4fcb645173ada931
satanxxzero <slavadorgamboa09@gmail.com>
2022-08-05 16:21:07 -0600

    first commit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase} from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8gNyMGk2XS8axBVJ7Koh9cDPmSA5PPAY",
-  authDomain: "actividad-15-1c02d.firebaseapp.com",
-  databaseURL: "https://actividad-15-1c02d-default-rtdb.firebaseio.com",
-  projectId: "actividad-15-1c02d",
-  storageBucket: "actividad-15-1c02d.appspot.com",
-  messagingSenderId: "949898823742",
-  appId: "1:949898823742:web:68163a577caf3db2dbe96f"
+  apiKey: "AIzaSyDrXCk82CYSFlWDElNVKrJbcJ9eEUzGHXo",
+  authDomain: "actividad-15-michelle-gamboa.firebaseapp.com",
+  databaseURL: "https://actividad-15-michelle-gamboa-default-rtdb.firebaseio.com",
+  projectId: "actividad-15-michelle-gamboa",
+  storageBucket: "actividad-15-michelle-gamboa.appspot.com",
+  messagingSenderId: "788103844471",
+  appId: "1:788103844471:web:4842b6ee666df4623dac72"
 };
 
 const db = initializeApp(firebaseConfig);
